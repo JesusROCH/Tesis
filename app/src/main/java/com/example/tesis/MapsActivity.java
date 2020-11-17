f36b2c2dc8c4f10b7d952a45179f0b6bff5ff677
Jesus Ortiz <jortiz@unsa.edu.pe>
2020-11-17 01:01:50 -0500

    Optimizando rutas de transporte publico y del usuario

--- app/src/main/java/com/example/tesis/MapsActivity.java
+++ app/src/main/java/com/example/tesis/MapsActivity.java
@@ -15,7 +15,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,6 +41,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private List<Marker> destinationMarkers = new ArrayList<>();
     private List<Polyline> polylinePaths = new ArrayList<>();
     private ProgressDialog progressDialog;
+    private int rutacc08idaC, rutacc08vueltaC;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,10 +70,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length >0){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 sendRequest();
-            }else{
+            } else {
                 Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
             }
         }
@@ -92,7 +92,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         try {
-            new DirectionFinder(this, origin, destination).execute();
+            new DirectionFinder(this, origin + "arequipa", destination + "arequipa").execute();
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
@@ -102,6 +102,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
@@ -159,7 +160,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     color(Color.GREEN)
                     .width(10);
 
-            PolylineOptions rutaCC08ida = new PolylineOptions()
+            PolylineOptions rutaCC08 = new PolylineOptions()
                     .add(new LatLng(-16.414941, -71.492536))
                     .add(new LatLng(-16.415216, -71.494284))
                     .add(new LatLng(-16.416492, -71.494072))
@@ -182,25 +183,194 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .add(new LatLng(-16.408325, -71.533418))
                     .add(new LatLng(-16.407660, -71.533937))
                     .add(new LatLng(-16.405869, -71.531649))
+                    .add(new LatLng(-16.405891, -71.531619))
+                    .add(new LatLng(-16.406521, -71.531150))
+                    .add(new LatLng(-16.407015, -71.530597))
+                    .add(new LatLng(-16.404406, -71.527456))
+                    .add(new LatLng(-16.404818, -71.527038))
+                    .add(new LatLng(-16.405201, -71.526531))
+                    .add(new LatLng(-16.406503, -71.525238))
+                    .add(new LatLng(-16.407023, -71.525997))
+                    .add(new LatLng(-16.407144, -71.526429))
+                    .add(new LatLng(-16.407515, -71.527124))
+                    .add(new LatLng(-16.407615, -71.527111))
+                    .add(new LatLng(-16.407898, -71.526864))
+                    .add(new LatLng(-16.408271, -71.526722))
+                    .add(new LatLng(-16.407955, -71.525810))
+                    .add(new LatLng(-16.407253, -71.524721))
+                    .add(new LatLng(-16.406893, -71.523831))
+                    .add(new LatLng(-16.406710, -71.523394))
+                    .add(new LatLng(-16.405750, -71.522112))
+                    .add(new LatLng(-16.404955, -71.520572))
+                    .add(new LatLng(-16.404022, -71.519490))
+                    .add(new LatLng(-16.404285, -71.519412))
+                    .add(new LatLng(-16.403894, -71.518717))
+                    .add(new LatLng(-16.403704, -71.517971))
+                    .add(new LatLng(-16.403346, -71.517288))
+                    .add(new LatLng(-16.403241, -71.516625))
+                    .add(new LatLng(-16.403141, -71.516459))
+                    .add(new LatLng(-16.403401, -71.516435))
+                    .add(new LatLng(-16.405520, -71.515537))
+                    .add(new LatLng(-16.405474, -71.515368))
+                    .add(new LatLng(-16.404609, -71.513930))
+                    .add(new LatLng(-16.405813, -71.512074))
+                    .add(new LatLng(-16.406634, -71.510770))
+                    .add(new LatLng(-16.407493, -71.509424))
+                    .add(new LatLng(-16.408396, -71.508013))
+                    .add(new LatLng(-16.408772, -71.507369))
+                    .add(new LatLng(-16.410192, -71.505263))
+                    .add(new LatLng(-16.410362, -71.504973))
+                    .add(new LatLng(-16.411019, -71.503995))
+                    .add(new LatLng(-16.412185, -71.502238))
+                    .add(new LatLng(-16.413464, -71.500309))
+                    .add(new LatLng(-16.414810, -71.498252))
+                    .add(new LatLng(-16.415352, -71.497419))
+                    .add(new LatLng(-16.415648, -71.497140))
+                    .add(new LatLng(-16.416891, -71.496652))
+                    .add(new LatLng(-16.416515, -71.494077))
+                    .add(new LatLng(-16.415213, -71.494270))
+                    .add(new LatLng(-16.414922, -71.492446))
+                    .add(new LatLng(-16.414359, -71.492567))
                     .geodesic(true)
-                    .color(Color.BLUE)
+                    .color(Color.GRAY)
                     .width(10);
 
-            PolylineOptions rutaCC08vuelta = new PolylineOptions()
-                    .add(new LatLng(-16.414941, -71.492536))
-                    .add(new LatLng(-16.415215, -71.494276))
-                    .geodesic(true)
-                    .color(Color.RED)
-                    .width(10);
+            originMarkers.add(mMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN))
+                    .title("Empresa 15 de Agosto S.A.")
+                    .snippet("Ruta C C08")
+                    .position(rutaCC08.getPoints().get(0))));
 
-            for (int i = 0; i < route.points.size(); i++)
-                polylineOptions.add(route.points.get(i));
+//            PolylineOptions rutaCC08vuelta = new PolylineOptions()
+//
+//                    .geodesic(true)
+//                    .color(Color.RED)
+//                    .width(10);
+//            originMarkers.add(mMap.addMarker(new MarkerOptions()
+//                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN))
+//                    .title("Empresa 15 de Agosto S.A.")
+//                    .snippet("Ruta C C08 de Vuelta")
+//                    .position(rutaCC08vuelta.getPoints().get(0))));
+
+            System.out.println("Tamaño transporte publico" + rutaCC08.getPoints().size());
+            //System.out.println("Tamaño transporte publico" + rutaCC08vuelta.getPoints().size());
+            System.out.println("Tamaño usuario" + route.points.size());
+
+            //RUTA C C08 DE IDA
+            if (rutaCC08.getPoints().size() > route.points.size()) {
+                int p = 0;
+                for (int i = 0; i < rutaCC08.getPoints().size(); i++) {
+                    if (route.points.get(p) == null) {
+                        p = 0;
+                    } else if (route.points.get(p) == rutaCC08.getPoints().get(i)) {
+                        System.out.println("Coincidencia: " + rutacc08idaC);
+                        rutacc08idaC++;
+                        p++;
+                    }
+                    polylineOptions.add(route.points.get(p));
+                }
+            } else if (rutaCC08.getPoints().size() < route.points.size()) {
+                for (int i = 0, p = 0; i < route.points.size(); i++) {
+                    if (rutaCC08.getPoints().get(p) == null) {
+                        p = 0;
+                    } else if (route.points.get(i) == rutaCC08.getPoints().get(p)) {
+                        System.out.println("Coincidencia: " + rutacc08idaC);
+                        rutacc08idaC++;
+                        p++;
+                    }
+                    polylineOptions.add(route.points.get(i));
+                }
+            } else if (rutaCC08.getPoints().size() == route.points.size()) {
+                for (int i = 0; i < route.points.size(); i++) {
+                    if (route.points.get(i) == rutaCC08.getPoints().get(i)) {
+                        System.out.println("Coincidencia: " + rutaCC08);
+                        rutacc08idaC++;
+                    }
+                    polylineOptions.add(route.points.get(i));
+                }
+            }
+            //RUTA C C08 DE VUELTA
+//            if (rutaCC08vuelta.getPoints().size() > route.points.size()) {
+//                int p = 0;
+//                for (int i = 0; i < rutaCC08vuelta.getPoints().size(); i++) {
+//                    if (route.points.get(p) == null) {
+//                        p = 0;
+//                    } else if (route.points.get(p) == rutaCC08vuelta.getPoints().get(i)) {
+//                        System.out.println("Coincidencia: " + rutacc08vueltaC);
+//                        rutacc08vueltaC++;
+//                        p++;
+//                    }
+//                    polylineOptions.add(route.points.get(p));
+//                }
+//            } else if (rutaCC08vuelta.getPoints().size() < route.points.size()) {
+//                for (int i = 0, p = 0; i < route.points.size(); i++) {
+//                    if (rutaCC08vuelta.getPoints().get(p) == null) {
+//                        p = 0;
+//                    } else if (route.points.get(i) == rutaCC08vuelta.getPoints().get(p)) {
+//                        System.out.println("Coincidencia: " + rutacc08vueltaC);
+//                        rutacc08vueltaC++;
+//                        p++;
+//                    }
+//                    polylineOptions.add(route.points.get(i));
+//                }
+//            } else if (rutaCC08vuelta.getPoints().size() == route.points.size()) {
+//                for (int i = 0; i < route.points.size(); i++) {
+//                    if (route.points.get(i) == rutaCC08vuelta.getPoints().get(i)) {
+//                        System.out.println("Coincidencia: " + rutacc08vueltaC);
+//                        rutacc08vueltaC++;
+//                    }
+//                    polylineOptions.add(route.points.get(i));
+//                }
+//            }
+
 
             polylinePaths.add(mMap.addPolyline(polylineOptions));
-            polylinePaths.add(mMap.addPolyline(rutaCC08ida));
-
-            //mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(-16.414941, -71.492536)));
+            polylinePaths.add(mMap.addPolyline(rutaCC08));
+            //polylinePaths.add(mMap.addPolyline(rutaCC08vuelta));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(route.startLocation, 20));
         }
+    }
+    //                float distancia = formulaHaversine(route.points.get(i), rutaCC08ida.getPoints().get(0));
+//                System.out.println("Distancia: " + distancia);
+
+//            if (rutaCC08ida.getPoints().size() < route.points.size()) {
+//                for (int i = 0; i < rutaCC08ida.getPoints().size(); i++) {
+//                    float distancia = formulaHaversine(route.points.get(i), rutaCC08ida.getPoints().get(i));
+//                    System.out.println("Distancia: " + distancia);
+////                    if (route.points.get(i) == rutaCC08ida.getPoints().get(i)) {
+////                        System.out.println("Puntos de Latitud y Logitud Iguales");
+////                    }
+//                    polylineOptions.add(route.points.get(i));
+//                }
+//            } else if (rutaCC08ida.getPoints().size() > route.points.size()) {
+//
+//            } else {
+//                for (int i = 0; i < route.points.size(); i++) {
+//                    float distancia = formulaHaversine(route.points.get(i), rutaCC08ida.getPoints().get(i));
+//                    System.out.println("Distancia: " + distancia);
+////                    if (route.points.get(i) == rutaCC08ida.getPoints().get(i)) {
+////                        System.out.println("Puntos de Latitud y Logitud Iguales");
+////                    }
+//                    polylineOptions.add(route.points.get(i));
+//                }
+//            }
+
+
+    private float formulaHaversine(LatLng latlng, LatLng latLng1) {
+        float radioTierra = 6378.0F;
+        float difLatitut = convertirRadianes(latlng.latitude - latLng1.latitude);
+        float difLongitude = convertirRadianes(latlng.longitude - latLng1.longitude);
+        float a = alCuadrado(Math.sin(difLatitut / 2)) + alCuadrado(Math.cos(convertirRadianes(latlng.latitude))) + alCuadrado(Math.cos(convertirRadianes(latLng1.latitude))) + alCuadrado(Math.sin(difLongitude / 2));
+        float c = (float) (2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)));
+
+        return radioTierra * convertirRadianes(c);
+    }
+
+    private float alCuadrado(double valor) {
+        return (float) Math.pow(valor, 2);
+    }
+
+    private float convertirRadianes(double v) {
+        return (float) ((float) Math.toDegrees(Math.PI / 180) * v);
     }
 }
